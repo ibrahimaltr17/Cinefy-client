602783900164d5d09781028ba742c94a6da6dd2e
ibrahimaltr17 <ibrahimaltr17@gmail.com>
2026-04-16 22:33:59 +0600

    Basic setup complete

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../shared/Navbar/Navbar';
+import Footer from '../shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
         <div className='max-w-7xl'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
